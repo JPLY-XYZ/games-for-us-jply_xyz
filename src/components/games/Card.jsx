4bb1e9434f066c51d1a9b7768664f44ef13d4688
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-02-20 13:20:42 +0100

    testeo

--- src/components/games/Card.jsx
+++ src/components/games/Card.jsx
@@ -2,7 +2,7 @@
 import GameButtons from "./Buttons";
 import PlatformsIcons from "./PlatformsIcons";
 
-function Card({ mediaQuery, title, releaseDate, imgUrl }) {
+function Card({ mediaQuery, title, releaseDate, imgUrl, platforms }) {
   return (
     <article
       className={
@@ -13,7 +13,7 @@ function Card({ mediaQuery, title, releaseDate, imgUrl }) {
       <div className="relative overflow-hidden rounded-xl">
         <img className="object-cover w-full h-full" src={imgUrl} alt={title} />
       </div>
-      <PlatformsIcons />
+      <PlatformsIcons platforms={platforms} />
       <div className="mx-auto p-2">
         <h2 className="text-slate-700 text-2xl font-bold uppercase">{title}</h2>
         <p className="mt-1 text-sm text-slate-400">{releaseDate}</p>
