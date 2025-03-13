23dc26b713f0e68f3bd1f424af4e4d54015a4d7e
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-03-13 15:00:46 +0100

    cambios menores

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound() {
    }, []) 
   return (
     <div className="not-found-container">
-      <img src="/src/assets/404.png" alt="Gif de un astronauta perdido" />
+      <img src="/404.png" alt="Gif de un astronauta perdido" />
       <h1 className="not-found-title text-3xl font-bold text-white">Página no encontrada</h1>
       <p className="not-found-message text-xl text-white">Lo sentimos, la página que buscas no existe.</p>
       <Link to="/" className=" cursor-pointer not-found-link text-2xl font-semibold text-white hover:underline">Volver al inicio</Link>
